3283948ee7f4a835f0a148e3f40677bd449c09e8
dntan7601 <dntan7601@gmail.com>
2022-08-31 20:52:18 +0700

    first commit

---
+++ credentials.js
@@ -0,0 +1,11 @@
+module.exports = {
+    cookieSecret: "your cookie secret goes here",
+    mongo: {
+        development: {
+            connectionString: 'mongodb+srv://daonhattan0706001:Daonhattan0706@cluster0.0csnm4w.mongodb.net/ViDienTu',
+        },
+        production: {
+            connectionString: 'your production_connection_string',
+        },
+    },
+}
